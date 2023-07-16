43ec989a161eb4e60db0272470b850d63855445f
jonathaneche <jonathan.eche@gmail.com>
2023-07-17 00:45:35 +0200

    rutas delete product, add product, obtener todos los productos de ua categoria

--- src/models.py
+++ src/models.py
@@ -11,3 +11,58 @@ def get_categories():
     data = [{'idcat': dato[0], 'descripcion': dato[1]} for dato in categorias]
     conn.close()
     return jsonify(data)
+
+#Delete one category by its id
+def delete_category_by_id(idcat):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM categorias WHERE idcat = %s', (idcat,))
+    conn.commit()
+    conn.close()
+    print("The category was deleted !!")
+    return ""
+
+#Get all products grouped by category
+def get_products_by_category(idcat):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM productos where idcat = %s', (idcat,))
+    productos = cur.fetchall()
+    data = [{'nombre': dato[1], 'precio': dato[2], 'img': dato[3]} for dato in productos]
+    conn.close()
+    return jsonify(data)
+
+#Get one product by id
+def get_product_by_id(id):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM productos WHERE id = %s', (id,))
+    dato_producto = cur.fetchone()
+    conn.close()
+    
+    if dato_producto:
+        producto = {
+            'nombre': dato_producto[1],
+            'precio': dato_producto[2],
+            'img': dato_producto[3]
+        }
+        return jsonify(producto)
+    else:
+        return 'El producto no fue encontrado'
+    
+#Agregar un producto
+def add_producto():
+    conn = connectdb()
+    cur = conn.cursor()
+    data = request.get_json()
+
+    nombre = data['nombre']
+    precio = data['precio']
+    img = data['img']
+    idcat = data['idcat']
+
+    cur.execute('INSERT INTO productos (nombre, precio, img, idcat) VALUES (%s, %s, %s, %s)', (nombre, precio, img, idcat))
+    conn.commit()
+    conn.close()                                                            
+    print('Producto agregado ')                                 
+    return "Producto agregado"
--- src/routes.py
+++ src/routes.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from src.models import *
-from src.models_euskera import *
+
 
 
 
@@ -13,22 +13,34 @@ CORS(app)
 def bienvenido():
     return "Bienvenido al proyecto Escuela Montaño"
 
-#get all categories in spanish
+#get all categories
 @app.route("/categorias", methods=['GET'])
 def all_categories():
     data = get_categories()
     return data
 
-#get all categories in basque
-@app.route("/kategoriak", methods=['GET'])
-def all_categories_eus():
-    data = get_categories_eus()
+#delete one categorie by id
+@app.route("/categorias/<idcat>", methods=['DELETE'])
+def delete_category(idcat):
+    delete_category_by_id(idcat)
+    return ""
+
+#Get all products grouped by category
+@app.route("/productos/<idcat>", methods=['GET'])
+def get_products(idcat):
+    data = get_products_by_category(idcat)
     return data
 
-#delete one categorie by id
-@app.route("/kategoriak/<idcat>", methods=['DELETE'])
-def delete_category_eus(idcat):
-    delete_category_by_id(idcat)
+#Get all products grouped by category
+@app.route("/producto_by_id/<id>", methods=['GET'])
+def get_product(id):
+    data = get_product_by_id(id)
+    return data
+
+#Add a product
+@app.route("/add_producto", methods=['POST'])
+def new_product():
+    add_producto()
     return ""
 
 if __name__ == "__main__":
