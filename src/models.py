a38e4d0f45ee1fdfab950d749432684d5edd3472
jonathaneche <jonathan.eche@gmail.com>
2023-07-17 10:27:01 +0200

    Rutas agregadas

--- src/models.py
+++ src/models.py
@@ -12,6 +12,21 @@ def get_categories():
     conn.close()
     return jsonify(data)
 
+#Agregar una categoria
+def add_categoria():
+    conn = connectdb()
+    cur = conn.cursor()
+    data = request.get_json()
+
+    idcat = data['idcat']
+    descripcion = data['descripcion']
+
+    cur.execute('INSERT INTO categorias (idcat, descripcion) VALUES (%s, %s)', (idcat, descripcion))
+    conn.commit()
+    conn.close()                                                            
+    print('Categoria agregada')                                 
+    return "Categoria agregada"
+
 #Delete one category by its id
 def delete_category_by_id(idcat):
     conn = connectdb()
@@ -22,15 +37,7 @@ def delete_category_by_id(idcat):
     print("The category was deleted !!")
     return ""
 
-#Get all products grouped by category
-def get_products_by_category(idcat):
-    conn = connectdb()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM productos where idcat = %s', (idcat,))
-    productos = cur.fetchall()
-    data = [{'nombre': dato[1], 'precio': dato[2], 'img': dato[3]} for dato in productos]
-    conn.close()
-    return jsonify(data)
+#########Funciones para tabla productos##############
 
 #Get one product by id
 def get_product_by_id(id):
@@ -66,3 +73,55 @@ def add_producto():
     conn.close()                                                            
     print('Producto agregado ')                                 
     return "Producto agregado"
+
+#Update un producto
+
+def update_producto(id):
+    conn = connectdb()
+    cur = conn.cursor()
+
+    data = request.get_json()
+
+    if "nombre" in data:
+        nombre = data["nombre"]
+        cur.execute('UPDATE productos SET nombre = %s WHERE id = %s', (nombre, id))
+
+    if "precio" in data:
+        precio = data["precio"]
+        cur.execute('UPDATE productos SET precio = %s WHERE id = %s', (precio, id))
+
+    if "img" in data:
+        img = data["img"]
+        cur.execute('UPDATE productos SET img = %s WHERE id = %s', (img, id))
+
+    if "idcat" in data:
+        idcat = data["idcat"]
+        cur.execute('UPDATE productos SET idcat = %s WHERE id = %s', (idcat, id))
+      
+    conn.commit()
+    conn.close()
+
+    return 'Producto modified'
+
+
+#Delete one product by its id
+def delete_producto_by_id(id):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM productos WHERE id = %s', (id,))
+    conn.commit()
+    conn.close()
+    print("The prodcut was deleted !!")
+    return "The product was deleted !!"
+
+
+
+#Get all products grouped by category
+def get_products_by_category(idcat):
+    conn = connectdb()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM productos where idcat = %s', (idcat,))
+    productos = cur.fetchall()
+    data = [{'nombre': dato[1], 'precio': dato[2], 'img': dato[3]} for dato in productos]
+    conn.close()
+    return jsonify(data)
--- src/routes.py
+++ src/routes.py
@@ -19,11 +19,17 @@ def all_categories():
     data = get_categories()
     return data
 
-#delete one categorie by id
+#Add a categoria - 
+@app.route("/add_categoria", methods=['POST'])
+def new_categoria():
+    add_categoria()
+    return "Categoria agregada"
+
+#delete one categorie by id -
 @app.route("/categorias/<idcat>", methods=['DELETE'])
 def delete_category(idcat):
     delete_category_by_id(idcat)
-    return ""
+    return "Category deleted"
 
 #Get all products grouped by category
 @app.route("/productos/<idcat>", methods=['GET'])
@@ -31,17 +37,30 @@ def get_products(idcat):
     data = get_products_by_category(idcat)
     return data
 
-#Get all products grouped by category
+#Get one product - funciona
 @app.route("/producto_by_id/<id>", methods=['GET'])
 def get_product(id):
     data = get_product_by_id(id)
     return data
 
-#Add a product
+#Add a product - funciona
 @app.route("/add_producto", methods=['POST'])
 def new_product():
     add_producto()
     return ""
+
+#Update one product - funciona
+@app.route("/update_producto/<id>", methods=['PATCH'])
+def update_product(id):
+    data= update_producto(id)
+    return data
+
+#Delete one product - funciona
+@app.route("/delete_producto/<id>", methods=['DELETE'])
+def delete_product(id):
+    delete_producto_by_id(id)
+    return "Producto was deleted"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
