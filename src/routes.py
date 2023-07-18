9a8fadecf4c2bfb802827dbc08a0447e4fe7133d
jonathaneche <jonathan.eche@gmail.com>
2023-07-18 10:53:55 +0200

    las rutas funcionan y rutas obtener todos los productos agregados

--- src/routes.py
+++ src/routes.py
@@ -13,25 +13,25 @@ CORS(app)
 def bienvenido():
     return "Bienvenido al proyecto Escuela Montaño"
 
-#get all categories
+#get all categories - Funciona
 @app.route("/categorias", methods=['GET'])
 def all_categories():
     data = get_categories()
     return data
 
-#Add a categoria - 
+#Add a categoria - Funciona
 @app.route("/add_categoria", methods=['POST'])
 def new_categoria():
     add_categoria()
     return "Categoria agregada"
 
-#delete one categorie by id -
+#delete one categorie by id - funciona
 @app.route("/categorias/<idcat>", methods=['DELETE'])
 def delete_category(idcat):
     delete_category_by_id(idcat)
     return "Category deleted"
 
-#Get all products grouped by category
+#Get all products grouped by category - funciona
 @app.route("/productos/<idcat>", methods=['GET'])
 def get_products(idcat):
     data = get_products_by_category(idcat)
@@ -43,11 +43,17 @@ def get_product(id):
     data = get_product_by_id(id)
     return data
 
+# Get all products -funciona
+@app.route("/all_products", methods=['GET'])
+def get_productos():
+    data = get_todo_products()
+    return data
+
 #Add a product - funciona
 @app.route("/add_producto", methods=['POST'])
 def new_product():
     add_producto()
-    return ""
+    return "Producto agregado"
 
 #Update one product - funciona
 @app.route("/update_producto/<id>", methods=['PATCH'])
@@ -64,42 +70,38 @@ def delete_product(id):
 
 ##########Rutas Usuarios#############
 
-#Add user
+#Add user - funciona
 @app.route("/add_user", methods=['POST'])
 def new_user():
     add_user()
     return "Usuario agregado"
 
 
-#get one user by id
+#get one user by id - funciona
 @app.route("/get_user/<id>", methods=['GET'])
 def obtener_user(id):
     data = get_user(id)
     return data
 
-#get all users
+#get all users - funciona
 @app.route("/usuarios", methods=['GET'])
 def all_users():
     data = get_usuarios()
     return data
 
 
-#update one user by id
+#update one user by id - funciona
 @app.route("/update_usuario/<id>", methods=['PATCH'])
 def update_usuario_route(id):
     data = update_usuario(id)
     return data
 
 
-
-
-#delete a user
+#delete a user - funciona
 @app.route("/delete_usuario/<id>", methods=['DELETE'])
 def delete_usuario(id):
     delete_usuario_by_id(id)
     return "Usuario was deleted"
-
-
 
 
 if __name__ == "__main__":
